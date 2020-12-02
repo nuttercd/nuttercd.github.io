81f31ca8f9cc3a5c841d763b457cf827f1c43009
nuttercd <colenutter@gmail.com>
2020-12-02 11:45:21 -0500

    Start data.js

--- data.js
+++ data.js
@@ -1,1 +1,12 @@
 "use strict";
+
+
+function saveArticle() {
+
+    form = document.querySelectorAll(".articleForm")
+    article = {"title":document.querySelectorAll(".articleTitle").value, "text":document.querySelectorAll(".articleText").value};
+
+}
+
+let saveButton = document.querySelectorAll(".button-save-article");
+saveButton.addEventListener("click", saveArticle());
